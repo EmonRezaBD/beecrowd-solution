e08f475c28127e34454f306781728e54f06f9b68
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-07-18 22:28:30 +0600

    Create  1096 - Sequence IJ 2.cpp

---
+++ 1096 - Sequence IJ 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=9;i=i+2)
+    {
+        for(int j=7;j>=5;j--)
+          cout<<"I="<<i<<" "<<"J="<<j<<endl;
+        
+    }
+    return 0;
+
+}
