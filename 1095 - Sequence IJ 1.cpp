0c0b189d91efc72fa04e5d937ba8f434955d403e
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-07-18 22:29:35 +0600

    Create  1095 - Sequence IJ 1.cpp

---
+++ 1095 - Sequence IJ 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    for(int i=1,j=60;j>-1;i=i+3,j=j-5)
+    {
+        cout<<"I="<<i<<" "<<"J="<<j<<endl;
+    }
+    return 0;
+
+}
