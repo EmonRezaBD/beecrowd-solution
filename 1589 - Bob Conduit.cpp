2e4106e65a7012ecca9bdf351b7ed809f0e37220
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-07-20 16:17:42 +0600

    Create 1589 - Bob Conduit.cpp

---
+++ 1589 - Bob Conduit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T,r1,r2;
+    cin>>T;
+    while(T--)
+    {
+        cin>>r1>>r2;
+        cout<<r1+r2<<endl;
+    }
+    return 0;
+}
