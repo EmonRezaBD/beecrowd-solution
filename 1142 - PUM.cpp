3c15ff1e0559a21606e43d29c88910a9ed8c364a
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-07-24 15:37:36 +0600

    Create  1142 - PUM.cpp

---
+++ 1142 - PUM.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    int I=1,J=2,K=3;
+    cin>>N;
+    while(N--)
+    {
+        cout<<I<<" "<<J<<" "<<K<<" "<<"PUM"<<endl;
+        I+=4;
+        J+=4;
+        K+=4;
+    }
+        return 0;
+    
+}
