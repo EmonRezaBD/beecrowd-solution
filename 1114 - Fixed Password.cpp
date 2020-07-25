bf1d465f2f5b2c6a72fbc729f19414678512356a
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-07-25 19:54:12 +0600

    Create  1114 - Fixed Password.cpp

---
+++ 1114 - Fixed Password.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    while(1)
+    {
+        cin>>N;
+        if(N==2002)
+        {
+            cout<<"Acesso Permitido"<<endl;
+            break;
+        }
+        else
+        {
+            cout<<"Senha Invalida"<<endl;
+            
+        }
+        
+    }
+        return 0;
+    
+}
