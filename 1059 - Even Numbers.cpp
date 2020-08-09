443689bd1265a77bdb57ceee9e5ebba79de7d845
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-08-09 16:10:38 +0600

    Create 1059 - Even Numbers.cpp

---
+++ 1059 - Even Numbers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=100;i++)
+    {
+        if(i%2==0)
+        printf("%d\n",i);
+    }
+    return 0;
+    //Here is the end of this program.
+}
