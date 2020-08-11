bf3aa58f87ed4dfccbc2c403cbb842d17c6adcd1
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-08-11 13:01:14 +0600

    Create 1017 - Fuel Spent.cpp

---
+++ 1017 - Fuel Spent.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int t,v,d;
+    float f;
+    scanf("%d %d",&t,&v);
+    d=(t*v);
+    f=d/12.0;
+    printf("%.3f\n",f);
+    return 0;
+    //Here is the end of this program.
+}
