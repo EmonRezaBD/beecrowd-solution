3223168bceea8edc198f39794ef1c57906dcf641
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-08-11 13:02:34 +0600

    Create 1014 - Consumption.cpp

---
+++ 1014 - Consumption.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int X;
+    float Y,avg;
+    scanf("%d %f",&X,&Y);
+    avg=X/Y;
+    printf("%.3f km/l\n",avg);
+    return 0;
+    //Here is the end of this program.
+}
