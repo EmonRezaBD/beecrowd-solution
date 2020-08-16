4712dcd1fce7e43889f497d0e8313eb7f945cc7c
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-08-16 11:50:29 +0600

    Create  1011 - Sphere.cpp

---
+++ 1011 - Sphere.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    double R,V,pi=3.14159;
+    scanf("%lf",&R);
+    V=(4/3.0)*pi*pow(R,3);
+    printf("VOLUME = %.3lf\n",V);
+    return 0;
+    //Here is the end of this program.
+}
