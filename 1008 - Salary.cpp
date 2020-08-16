db7dc12ed00c589d29f5068fda66152048609167
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-08-16 11:51:11 +0600

    Create  1008 - Salary.cpp

---
+++ 1008 - Salary.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num,wh;
+    float phs,total;
+    scanf("%d %d",&num,&wh);
+    scanf("%f",&phs);
+    total=(wh*phs);
+    printf("NUMBER = %d\n",num);
+    printf("SALARY = U$ %.2f\n",total);
+    return 0;
+    //Here is the end of this program.
+}
