9ecd9a85a979c436ac755a8dda8c294cb0842de1
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-08-18 09:42:51 +0600

    Create 1007 - Difference.cpp

---
+++ 1007 - Difference.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,C,D,diff;
+    scanf("%d %d %d %d",&A,&B,&C,&D);
+    diff=(A*B-C*D);
+    printf("DIFERENCA = %d\n",diff);
+    return 0;
+    //Here is the end of this program.
+}
